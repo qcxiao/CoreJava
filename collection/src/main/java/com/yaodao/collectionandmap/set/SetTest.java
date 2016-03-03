78a975e675d656b9ee441727c3f8b2c226e01d8a
qcxiao <280887262@qq.com>
2016-03-03 22:24:14 +0800

    u

--- collection/src/main/java/com/yaodao/collectionandmap/set/SetTest.java
+++ collection/src/main/java/com/yaodao/collectionandmap/set/SetTest.java
@@ -1,12 +1,8 @@
 package com.yaodao.collectionandmap.set;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Random;
 import java.util.Set;
-
-import com.sun.corba.se.spi.transport.CorbaTransportManager;
 
 /**
  * Set集合里的对象排列是无序的，并且不会有重复对象
